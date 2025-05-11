5db57bdeced7b487672b5e42bcf59d62765a9d2f
Guillaume Cornut <guillaume.cornut@lumapps.com>
2025-05-11 11:29:43 +0200

    Move utils functions

--- flake.nix
+++ flake.nix
@@ -29,21 +29,7 @@
         useGlobalPkgs = true;
         useUserPackages = false;
         users."${user}" = import ./home;
-        extraSpecialArgs = {
-          mkDotfileLink = config: path:
-            let
-              rootStr = toString self;
-              pathStr = toString path;
-            in assert lib.assertMsg (lib.hasPrefix rootStr pathStr)
-              "${pathStr} does not start with ${rootStr}";
-              config.lib.file.mkOutOfStoreSymlink (builtins.getEnv "PWD") + (lib.removePrefix rootStr pathStr);
-          getEnv = var:
-            let
-              val = builtins.getEnv var;
-            in
-              assert lib.assertMsg (val != "") "Missing env var ${var}";
-              val;
-        };
+        extraSpecialArgs = import ./modules/utils.nix { inherit self lib; };
       };
     };
   in
